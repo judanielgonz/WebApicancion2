68f95c5f01b110d289c8d9102d228fd9d13c2c97
Juan daniel Gonzales Aramayo <a2020116081@estudiantes.upsa.edu.bo>
2021-10-18 17:40:37 -0400

    Migre los modelos hacia la base de datos

---
+++ Migrations/20211018213954_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApicancion2.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cancion",
+                columns: table => new
+                {
+                    Nombre = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Letra = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Enlace = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cancion", x => x.Nombre);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cancion");
+        }
+    }
+}
